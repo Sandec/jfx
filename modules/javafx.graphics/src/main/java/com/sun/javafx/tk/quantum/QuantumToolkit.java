89d66026b1067f1f8356fb0a27cc56c217f6b881
floriankirmaier <florian.kirmaier@gmail.com>
2021-09-15 23:04:57 +0200

    Added option to disable rendering and various optimizations

--- modules/javafx.graphics/src/main/java/com/sun/javafx/tk/quantum/QuantumToolkit.java
+++ modules/javafx.graphics/src/main/java/com/sun/javafx/tk/quantum/QuantumToolkit.java
@@ -192,8 +192,14 @@ public final class QuantumToolkit extends Toolkit {
                 return result;
             });
 
+    public static boolean renderOnlySnapshots =
+            AccessController.doPrivileged((PrivilegedAction<Boolean>) () -> {
+                Boolean result = Boolean.getBoolean("quantum.renderonlysnapshots");
+                return result;
+            });
+
     @SuppressWarnings("removal")
-    private static boolean noRenderJobs =
+    public static boolean noRenderJobs =
             AccessController.doPrivileged((PrivilegedAction<Boolean>) () -> {
                 Boolean result = Boolean.getBoolean("quantum.norenderjobs");
                 if (/*verbose &&*/ result) {
@@ -463,9 +469,13 @@ public final class QuantumToolkit extends Toolkit {
         return Toolkit.getFxUserThread();
     }
 
-    @Override public Future addRenderJob(RenderJob r) {
+    public Future addRenderJob(RenderJob r) {
+        return addRenderJob(r,false);
+    }
+
+    public Future addRenderJob(RenderJob r, boolean isSnapshot){
         // Do not run any render jobs (this is for benchmarking only)
-        if (noRenderJobs) {
+        if (noRenderJobs || (!isSnapshot && renderOnlySnapshots)) {
             CompletionListener listener = r.getCompletionListener();
             if (r instanceof PaintRenderJob) {
                 ((PaintRenderJob)r).getScene().setPainting(false);
@@ -1682,7 +1692,7 @@ public final class QuantumToolkit extends Toolkit {
 
         final CountDownLatch latch = new CountDownLatch(1);
         re.setCompletionListener(job -> latch.countDown());
-        addRenderJob(re);
+        addRenderJob(re, true);
 
         do {
             try {
