5d1e9a58dfe07302461adb1586d3af182c1bbd0e
Besmir Beqiri <bb@sandec.de>
2024-10-16 19:16:43 +0200

    Fix missing javadoc errors

--- modules/javafx.graphics/src/main/java/javafx/scene/Scene.java
+++ modules/javafx.graphics/src/main/java/javafx/scene/Scene.java
@@ -758,6 +758,11 @@ public class Scene implements EventTarget {
         postLayoutPulseListeners.add(r);
     }
 
+    /**
+     * Adds a listener to be called before each rendering pulse.
+     *
+     * @param r the {@code Runnable} to be called
+     */
     public final void addPreRenderPulseListener(Runnable r) {
         Toolkit.getToolkit().checkFxUserThread();
 
@@ -769,7 +774,6 @@ public class Scene implements EventTarget {
         }
         preRenderPulseListeners.add(r);
     }
-
 
     /**
      * Removes a previously registered scene post layout pulse listener from listening to
@@ -795,6 +799,11 @@ public class Scene implements EventTarget {
         postLayoutPulseListeners.remove(r);
     }
 
+    /**
+     * Removes a listener that was previously added to be called before each rendering pulse.
+     *
+     * @param r the {@code Runnable} to be removed
+     */
     public final void removePreRenderPulseListener(Runnable r) {
         Toolkit.getToolkit().checkFxUserThread();
 
