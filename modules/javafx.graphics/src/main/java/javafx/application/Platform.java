fa6b8c0c80f79fe6135cd4b6cd2c5d2efdbeecc4
floriankirmaier <florian.kirmaier@gmail.com>
2024-11-19 14:20:17 +0100

    Added option to overwrite ConditionalFeatures

--- modules/javafx.graphics/src/main/java/javafx/application/Platform.java
+++ modules/javafx.graphics/src/main/java/javafx/application/Platform.java
@@ -41,6 +41,9 @@ import javafx.scene.paint.Color;
  */
 public final class Platform {
 
+    /** This is a placeholder for the actual implementation. */
+    public static java.util.function.Function<ConditionalFeature,Optional<Boolean>> overwriteIsSupported = (feature) -> Optional.empty();
+
     // To prevent instantiation
     private Platform() {
     }
@@ -268,7 +271,7 @@ public final class Platform {
      * platform, otherwise false
      */
     public static boolean isSupported(ConditionalFeature feature) {
-        return PlatformImpl.isSupported(feature);
+        return overwriteIsSupported.apply(feature).orElseGet(() -> PlatformImpl.isSupported(feature));
     }
 
     /**
