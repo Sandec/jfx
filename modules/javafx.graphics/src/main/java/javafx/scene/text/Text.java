2d7774c3c2a553afc2c143f202df3fa64c6427e8
floriankirmaier <florian.kirmaier@gmail.com>
2024-12-11 16:09:51 +0100

    fix javadoc

--- modules/javafx.graphics/src/main/java/javafx/scene/text/Text.java
+++ modules/javafx.graphics/src/main/java/javafx/scene/text/Text.java
@@ -126,6 +126,10 @@ text.setText("The quick brown fox jumps over the lazy dog");
  */
 @DefaultProperty("text")
 public class Text extends Shape {
+
+    /**
+     * Used by jpro
+     */
     public java.lang.Runnable runsUpdatedLambda = () -> {};
     static {
         TextHelper.setTextAccessor(new TextHelper.TextAccessor() {
